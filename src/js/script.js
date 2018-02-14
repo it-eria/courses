1b6980561c38de788ab9d3cf64c14d71380a7d21
Vitaliy <pyzhyck@gmail.com>
2018-02-14 20:15:23 +0200

    14.02.2018

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 //= vendors/jquery-3.1.1.min.js
 //= vendors/slick.js
 //= vendors/jquery.nice-select.js
+//= vendors/jquery.datetimepicker.min.js
 
 var lFollowX = 0,
     lFollowY = 0,
@@ -28,7 +29,115 @@ $(window).on('mousemove click', function(e) {
   lFollowY = (10 * lMouseY) / 100;
 });
 
+// Customize calendar
+$.fn.datetimepicker.CONSTS = {
+  I18N: {
+      zh: {
+          SDN: ["日", "一", "二", "三", "四", "五", "六"],
+          MN: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+          DN: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
+          CALENDAR: "日历",
+          CLEAR: "清空",
+          TODAY: "今天",
+          OK: "确定",
+          CURRENT: "当前",
+          TIME: "时间"
+      },
+      en: {
+          SDN: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+          MN: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+          DN: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+          CALENDAR: "Calendar",
+          CLEAR: "Clear",
+          TODAY: "Today",
+          OK: "OK",
+          CURRENT: "Now",
+          TIME: "Time"
+      },
+      de: {
+          SDN: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+          MN: ["Jan", "Feb", "Mrz", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+          DN: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+          CALENDAR: "Kalender",
+          CLEAR: "Löschen",
+          TODAY: "Heute",
+          OK: "OK",
+          CURRENT: "Jetzt",
+          TIME: "Zeit"
+      },
+      ua: {
+        SDN: ["Нд", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+        MN: ["Січ", "Лют", "Бер", "Кві", "Тра", "Чер", "Лип", "Сер", "Вер", "Жов", "Лис", "Гру"],
+        DN: ["Неділя", "Понеділок", "Вівторок", "Середа", "Четверг", "П'ятниця", "Субота"],
+        CALENDAR: "Календар",
+        CLEAR: "Очистити",
+        TODAY: "Сьогодні",
+        OK: "OK",
+        CURRENT: "Зараз",
+        TIME: "Час"
+      }
+  },
+
+  VIEWMODE: {
+      YM: 'YM',  // yyyyMM
+      YMD: 'YMD', // yyyyMMdd
+      HMS: 'HMS', // HHmmss
+      HM: 'HM', // HHmm
+      YMDHMS: 'YMDHMS', // yyyyMMddHHmmss
+      YMDHM: 'YMDHM' // yyyyMMddHHmm
+  },
+
+  MINYEAR: 1900,
+  MAXYEAR: 2999,
+
+  NAV: {
+      'prevm': 2, //previous month
+      'nextm': 3, //next month
+      'title': 4, //title
+      'clear': 5, //clear
+      'today': 6, //today
+      'dok': 7,   //ok in day pane
+      'prevy': 8, //previous ten years
+      'nexty': 9, //next ten years
+      'cancel': 10, //cancel
+      'mok': 11,   //ok in month pane
+      'plus': 12, //plus
+      'minus': 13, //minus
+      'current': 15, //current
+      'day': 100, //day
+      'month': 200, //month
+      'year': 300 //year
+  }
+};
+
 $(function() {
+
+  var dateSelected;
+
+  if($('#calendar').length > 0) {
+    $('#calendar').datetimepicker({
+      baseCls: "perfect-datetimepicker", 
+      viewMode: 'YMD',
+      firstDayOfWeek: 1,
+      date: new Date(), //initial date
+      endDate: null, //end date
+      startDate: null, //start date
+      language: 'ua', //I18N
+      //date update event
+      onDateUpdate: null,
+      //clear button click event
+      onClear: null,
+      //ok button click event
+      onOk: function() {
+        dateSelected = this.getText().replace(/[0-9][0-9]\:[0-9][0-9]\:[0-9][0-9]/g, '');
+        $("#dateValue").val(dateSelected);
+      },
+      //close button click event
+      onClose: null,
+      //today button click event
+      onToday: null
+    });
+  }
 
   $('.preloader').delay(2100).fadeOut(300);
   setTimeout(function() {
@@ -49,7 +158,7 @@ $(function() {
         $('.main-nav').toggleClass('opened');
         $('.header *[data-js="blur"]').toggleClass('blur');
         if($(this).hasClass('active')) {
-          $('.header').css('min-height', $('.main-nav').height()+150);
+          $('.header').css('min-height', '100vh');
         } else {
           $('.header').removeAttr('style');
         }
